e185e9cee28ac6a5e635ab002630ea91017322c1
Praayassh <prayashs102@gmail.com>
2024-05-19 22:15:19 +0545

    Initial commit for this project

--- src/main/java/np/com/prayashsapkota/main/Main.java
+++
@@ -1,9 +0,0 @@
-package main.java.np.com.prayashsapkota.main;
-
-import main.java.np.com.prayashsapkota.functionality.BiteMoreFoodDeliverySystem;
-
-public class Main {
-    public static void main(String[] args) {
-        BiteMoreFoodDeliverySystem.runBiteMoreSystem();
-    }
-}
