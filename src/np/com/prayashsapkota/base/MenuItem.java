68d1bb49219727bc94f62bb167c84bd033110b00
Praayassh <prayashs102@gmail.com>
2024-05-19 22:24:10 +0545

    Initial commit for this project

---
+++ src/np/com/prayashsapkota/base/MenuItem.java
@@ -0,0 +1,35 @@
+package np.com.prayashsapkota.base;
+
+public class MenuItem {
+    private final String name;
+    private final double price;
+    private double quantity;
+
+
+    public MenuItem(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public double getTotalPrice() {
+        return getQuantity() * getPrice();
+    }
+
+    public void seQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+}
---
+++ src/np/com/prayashsapkota/main/Main.java
@@ -0,0 +1,9 @@
+package np.com.prayashsapkota.main;
+
+import np.com.prayashsapkota.functionality.BiteMoreFoodDeliverySystem;
+
+public class Main {
+    public static void main(String[] args) {
+        BiteMoreFoodDeliverySystem.runBiteMoreSystem();
+    }
+}
